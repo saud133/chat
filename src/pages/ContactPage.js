88ddeed5bbbe58a63bc5fb39c3bc11faf8b3ddd9
saud133 <saud133@hotmail.com>
2025-09-19 19:53:51 +0300

    input

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -67,7 +67,7 @@ const ContactPage = () => {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ 
           userId: userId,
-          message: userInput 
+          chatInput: userInput 
         })
       });
 
